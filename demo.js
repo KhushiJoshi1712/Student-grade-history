2d8e871b76ce708815c679aad12955e646f0fb1a
Khushi Joshi <127647768+KhushiJoshi1712@users.noreply.github.com>
2023-04-26 21:02:26 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,290 @@
+function show() {
+    const email = document.querySelector("#enrollment");
+    const passid = document.querySelector("#passid");
+    if(email.value==passid.value){
+        second.innerHTML = `
+        <br>
+        <table>
+          <tr>
+          <td>Name:</td>
+          <td>JOSHI KHUSHI SANJAYKUMAR</td>
+          </tr>
+          <tr>
+          <td>Enrollment No:</td>
+          <td>200200107092</td>
+          </tr>
+          <tr>
+          <td>Institute:</td>
+          <td>020-Government Engineering College ,Rajkot </td>
+          </tr>
+          <tr>
+          <td>Branch:</td>
+          <td>BE-07-COMPUTER ENGINEERING</td>
+          </tr>
+          <table>
+          </table>
+
+          <br>
+<table Border="1" width="100%">
+    <tr id="t30">
+    <td>Enrollment No.</td>
+    <td>CPI</td>
+    <td>CGPA</td>
+    <td>TOT BACKL</td>
+    <td>BCK1</td>
+    <td>BCK2</td>
+    <td>BCK3</td>
+    <td>BCK4</td>
+    <td>BCK5</td>
+    <td>BCK6</td>
+    <td>BCK7</td>
+    <td>BCK8</td>
+</tr>
+<tr id="t31">
+    <td>200200107092</td>
+    <td>8.47</td>
+    <td>0.00</td>
+    <td>0</td>
+    <td>0</td>
+    <td>0</td>
+    <td>0</td>
+    <td>-</td>
+    <td>-</td>
+    <td>-</td>
+    <td>-</td>
+</tr>
+</table>
+
+<br>
+
+<table Border="1" width="100%" id="t32">
+    <tr>
+        <td>Last exam</td>
+        <td>Sem</td>
+        <td>Declaration Date</td>
+        <td>Cur Back</td>
+        <td>SPI</td>
+    </tr>
+    <tr>
+        <td>BE SEM 4-Regular(MAY 2022)</td>
+        <td>4</td>
+        <td>16-9-2022</td>
+        <td>0</td>
+        <td>7.26</td>
+    </tr>
+</table>
+
+<br>
+<table border="1" width="100%" id="t33">
+<tr>
+<td>SEM 1</td>
+</tr>
+</table>
+
+<br>
+<table border="1" width="100%">
+<tr>
+<td>WINTER-2020(SEAT NO : 200200107092)</td>
+<td>CUR. BACKLOG : 0</td>
+</tr>
+<br>
+<table Border="1" width="100%" id="t34"
+
+        <table border="3">
+                    <tr>        
+                        <th>SUB Code</th>
+                        <th>SUBJECT NAME</th>
+                        <td>E_AB</td>
+                        <td>TH ESE</td>
+                        <td>TH PA</td>
+                        <td>TH TOTAL</td>
+                        <td>PR ESE</td>
+                        <td>PR PA</td>
+                        <td>PR TOTAL</td>
+                        <td>SUBJECT GRADE</td>
+                    </tr>
+                    <tr>
+                        <td>3110005</td>
+                        <td>Basic electrical engineering</td>
+                        <td>-</td>
+                        <td>AA</td>
+                        <td>AB</td>
+                        <td>AA</td>
+                        <td>AB</td>
+                        <td>AB</td>
+                        <td>AB</td>
+                        <td>AA</td>
+                     </tr>
+                    <tr>
+                        <td>3110006</td>
+                        <td>Basic mechanical engineering</td>
+                        <td>-</td>
+                        <td>AA</td>
+                        <td>AA</td>
+                        <td>AA</td>
+                        <td>AA</td>
+                        <td>AA</td>
+                        <td>AA</td>
+                        <td>AA</td>
+                    </tr>
+                    <tr>
+                        <td>3110012</td> 
+                        <td>Workshop/Manufacturing practices</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>AB</td>
+                        <td>AB</td>
+                        <td>AB</td>
+                        <td>AB</td>
+                        <td>AB</td>
+                        
+                    </tr>
+                    <tr>
+                        <td>3110014</td>
+                        <td>Mathematics-1</td>
+                        <td>-</td>
+                        <td>BB</td>
+                        <td>AB</td>
+                        <td>BB</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>BB</td>
+                    </tr>
+                    <tr>
+                        <td>3110017</td>
+                        <td>Induction program</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>PS</td>
+                    </tr>
+                    <tr>
+                        <td>3110018</td>
+                        <td>Physics</td>
+                        <td>-</td>
+                        <td>AA</td>
+                        <td>AA</td>
+                        <td>AA</td>
+                        <td>AA</td>
+                        <td>AA</td>
+                        <td>AA</td>
+                        <td>AA</td>
+                    </tr>
+                </table>
+                <br>
+<table border="1" width="100%" id="t33">
+<tr>
+<td>SEM 2</td>
+</tr>
+</table>
+
+<br>
+<table border="1" width="100%">
+<tr>
+<td>SUMMER-2021(SEAT NO : 200200107092)</td>
+<td>CUR. BACKLOG : 0</td>
+</tr>
+<br>
+<table Border="1" width="100%" id="t34"
+
+        <table border="3">
+                    <tr>        
+                        <th>SUB Code</th>
+                        <th>SUBJECT NAME</th>
+                        <td>E_AB</td>
+                        <td>TH ESE</td>
+                        <td>TH PA</td>
+                        <td>TH TOTAL</td>
+                        <td>PR ESE</td>
+                        <td>PR PA</td>
+                        <td>PR TOTAL</td>
+                        <td>SUBJECT GRADE</td>
+                    </tr>
+                    <tr>
+                        <td>3110002</td>
+                        <td>English</td>
+                        <td>-</td>
+                        <td>AA</td>
+                        <td>AB</td>
+                        <td>AA</td>
+                        <td>AB</td>
+                        <td>AB</td>
+                        <td>AB</td>
+                        <td>AA</td>
+                     </tr>
+                    <tr>
+                        <td>3110003</td>
+                        <td>programming for problem solving</td>
+                        <td>-</td>
+                        <td>AA</td>
+                        <td>AA</td>
+                        <td>AA</td>
+                        <td>AA</td>
+                        <td>AA</td>
+                        <td>AA</td>
+                        <td>AA</td>
+                    </tr>
+                    <tr>
+                        <td>3110017</td> 
+                        <td>Environmental science</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>AB</td>
+                        <td>AB</td>
+                        <td>AB</td>
+                        <td>AB</td>
+                        <td>AB</td>
+                        
+                    </tr>
+                    <tr>
+                        <td>3110013</td>
+                        <td>EGD</td>
+                        <td>-</td>
+                        <td>BB</td>
+                        <td>AB</td>
+                        <td>BB</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>BB</td>
+                    </tr>
+                    <tr>
+                        <td>3110015</td>
+                        <td>Mathematics 2</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>PS</td>
+                    </tr>
+                    <tr>
+                        <td>3110016</td>
+                        <td>Basic electronics</td>
+                        <td>-</td>
+                        <td>AA</td>
+                        <td>AA</td>
+                        <td>AA</td>
+                        <td>AA</td>
+                        <td>AA</td>
+                        <td>AA</td>
+                        <td>AA</td>
+                    </tr>
+                </table>
+                `;
+    }
+    else{
+        alert("plz enter correct info");
+    }
+
+  } 
+
